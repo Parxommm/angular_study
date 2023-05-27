17ddcb4a0fdca58c5f4ad6867a8e52e30ee039b0
Evgeny Parkhomenko <evg.parkhomenko@gmail.com>
2023-05-27 23:38:48 +0300

    fix: change path

--- mediumclone-angular/src/app/auth/components/register/register.component.ts
+++ mediumclone-angular/src/app/auth/components/register/register.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder } from '@angular/forms';
 import { Store } from '@ngrx/store';
-import { registerAction } from '../../store/actions/register.action.ts';
+import { registerAction } from '../../store/actions/register.action';
 
 @Component({
   selector: 'mc-register',
