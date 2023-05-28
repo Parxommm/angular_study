2b93dd02425817daf3b730bb60e6726349135e96
Evgeny Parkhomenko <evg.parkhomenko@gmail.com>
2023-05-28 12:14:59 +0300

    feat: implement authFeature selector

--- mediumclone-angular/src/app/auth/components/register/register.component.ts
+++ mediumclone-angular/src/app/auth/components/register/register.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder } from '@angular/forms';
-import { Store } from '@ngrx/store';
+import { Store, select } from '@ngrx/store';
 import { registerAction } from '../../store/actions/register.actions';
+import { Observable } from 'rxjs';
+import { isSubmittingSelector } from '../../store/selectors/authFeature.selectors';
 
 @Component({
   selector: 'mc-register',
@@ -10,11 +12,13 @@ import { registerAction } from '../../store/actions/register.actions';
 })
 export class RegisterComponent implements OnInit {
   form: FormGroup;
+  isSubmitting$: Observable<boolean>;
 
   constructor(private fb: FormBuilder, private store: Store) {}
 
   ngOnInit(): void {
     this.initializeForm();
+    this.initializeValues();
   }
 
   initializeForm(): void {
@@ -25,6 +29,11 @@ export class RegisterComponent implements OnInit {
     });
 
     console.log(this.form.valid);
+  }
+
+  initializeValues(): void {
+    this.isSubmitting$ = this.store.pipe(select(isSubmittingSelector));
+    console.log('isSubmitting: ', this.isSubmitting$);
   }
 
   onSubmit(): void {
