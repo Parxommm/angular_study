9ad8df60b21a38c2471ac0fe88b7cc27732ba79a
Evgeny Parkhomenko <evg.parkhomenko@gmail.com>
2023-09-06 14:04:36 +0300

    feat: implement error, loading and banner components

--- mediumclone-angular/src/app/shared/modules/feed/feed.module.ts
+++ mediumclone-angular/src/app/shared/modules/feed/feed.module.ts
@@ -7,6 +7,8 @@ import { StoreModule } from '@ngrx/store';
 import { feedStateFeature } from './store/reducers/feed-state.reducer';
 import { FeedService } from './services/feed.service';
 import { RouterModule } from '@angular/router';
+import { ErrorMessageModule } from '../error-message/error-message.module';
+import { LoadingModule } from '../banner/loading/loading.module';
 
 @NgModule({
   declarations: [FeedComponent],
@@ -15,6 +17,8 @@ import { RouterModule } from '@angular/router';
     EffectsModule.forFeature(GetFeedEffect),
     StoreModule.forFeature(feedStateFeature),
     RouterModule,
+    ErrorMessageModule,
+    LoadingModule,
   ],
   exports: [FeedComponent],
   providers: [FeedService],
